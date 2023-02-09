e2b9937e68d4914b37821d9573e71d88ae9a2f4b
HeloBaron <helo.athayde@gmail.com>
2023-02-08 21:38:46 -0300

    update: Add comment and delete post

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,12 @@ import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 
-export function Comment() {
+export function Comment({ content, deleteComment }) {
+    function handleDeleteComment() {
+
+        deleteComment(content)
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
@@ -16,12 +21,12 @@ export function Comment() {
                             <time title="02 de Fevereiro às 15:43" dateTime="2023-05-11 15:44">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={20} />
                         </button>
                     </header>
 
-                    <p>Muito bom, parabéns</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
